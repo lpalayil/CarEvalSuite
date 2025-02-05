b13f1cb8448096d511c29f80b360ef8a88368faf
lpalayil <lavanyap.nair@gmail.com>
2025-02-05 21:15:38 +0000

    Update test_carcheck.py
    Added comments and code-cleanup

--- Tests/test_carcheck.py
+++ Tests/test_carcheck.py
@@ -14,13 +14,12 @@ class TestCarValuation(BaseClass):
     def test_car_valuation(self, registration_number):
         """Test car valuation using the Motorway website."""
 
-        # Assume there's a field for car registration number input
         try:
             logger = self.get_logger()
             self.driver.get(CAR_VALUATION_URL)
             home_page = HomePage(self.driver)
-            #valuation_page = ValuationPage(self.driver)
-
+           
+            # Enter the registration number
             logger.info(f"Entering the Car registration number : {registration_number}")
             reg_input = home_page.enter_car_reg()
             reg_input.send_keys(registration_number)
@@ -36,15 +35,14 @@ class TestCarValuation(BaseClass):
             reg_data = valuation_page.get_registration_num().text
             print(reg_data)
 
+            # Extract the make model
             make_model = valuation_page.get_make_model().text
             print(make_model)
 
-
-            # ul_element = self.driver.find_element(By.CSS_SELECTOR,"ul[data-cy='vehicleSpecifics']")
-            #time.sleep(5)
+            # Extract the vehical specifics text
             vehicle_specifics = valuation_page.get_vehicle_specifics()
-            time.sleep(5)
 
+            #Read the vehical specific data into list
             vehicle_details = list()
             for specifics in vehicle_specifics:
                 vehicle_details.append(specifics.text.strip())
